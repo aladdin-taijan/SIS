b313344c3bb47fa59144d1d80a88aa61537f5415
Aladdin Taijan <aladdin.taijan@gmail.com>
2021-07-26 23:48:30 +0300

    add country list to controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Monarobase\CountryList\CountryList;
+use Monarobase\CountryList\CountryListFacade;
 
 class StudentController extends Controller
 {
 
     public function create(){
-        return view('student/info');
+
+        $country = CountryListFacade::getlist('ar');
+        return view('student/info',compact('country'));
     }
     public function store(Request $request)
     {
